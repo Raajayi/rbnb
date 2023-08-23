8e5bf10ef09d7a0bf43cf919e452549384f3daee
Indya Mandira <mandiraindy@gmail.com>
2023-08-23 14:41:05 +0800

    added availability seeds and edited booking seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,11 @@ Band.create!(name: "Radiohead",
 # band = Band.create!(name: "Pink Floyd", description: "An English rock band that became famous in the 1960s and 1970s. They performed for more than 30 years, and have sold more than 200 million albums.", genre: "Rock", photo_url:"https://upload.wikimedia.org/wikipedia/id/thumb/f/f3/PinkFloyd-album-piperatthegatesofdawn.jpg/640px-PinkFloyd-album-piperatthegatesofdawn.jpg")
 # band.user = user
 
-Booking.create!(start_time: "25-12-2018", end_time: "26-12-2018", user: User.first, band: Band.first)
-Booking.create!(start_time: "10-1-2019", end_time: "11-1-2019", user: User.second, band: Band.second)
-Booking.create!(start_time: "19-2-2019", end_time: "20-2-2019", user: User.third, band: Band.third)
+Booking.create!(start_time: "2.10 PM", end_time: "6.10 PM", user: User.first, band: Band.first)
+Booking.create!(start_time: "4.00 PM", end_time: "8.00 PM", user: User.second, band: Band.second)
+Booking.create!(start_time: "11.00 AM", end_time: "5.00 PM", user: User.third, band: Band.third)
+
+Availability.create!(start_time: "25-12-2018", end_time: "26-12-2018", band: Band.first)
+Availability.create!(start_time: "10-1-2019" , end_time: "11-1-2019", band: Band.first)
+Availability.create!(start_time: "19-2-2019", end_time: "20-2-2019", band: Band.first)
 
