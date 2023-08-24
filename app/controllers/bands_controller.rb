77debb0e71b5af89ee4e3d90b82c21602af1217d
Indya Mandira <mandiraindy@gmail.com>
2023-08-24 16:08:29 +0800

    added cloudinary image uploading to the create band form, fixed seeds and added bootstrap to the index page

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -29,6 +29,6 @@ class BandsController < ApplicationController
   end
 
   def band_params
-    params.require(:band).permit(:name, :description, :photo_url, :genre)
+    params.require(:band).permit(:name, :description, :photo_url, :genre, :photo)
   end
 end
--- app/models/band.rb
+++ app/models/band.rb
@@ -2,4 +2,5 @@ class Band < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :availabilities
+  has_one_attached :photo
 end
